3a5c760e5569a02ae10f891d114ca7a9721274e1
Jennifer Amaya <vane_amaya20@hotmail.es>
2025-07-08 20:13:00 -0400

    Revert DOOM app to use silentspacemarine.com iframe for maximum compatibility

--- src/components/applications/Doom.tsx
+++ src/components/applications/Doom.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Window from '../os/Window';
 import type { WindowAppProps } from '../../types/WindowAppProps';
-import DosPlayerNew from '../dos/DosPlayerNew';
 
 export interface DoomAppProps extends WindowAppProps {}
 
 const DoomApp: React.FC<DoomAppProps> = (props) => {
     const [width, setWidth] = useState(1000);
     const [height, setHeight] = useState(700);
+    const canvasRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            canvasRef.current.innerHTML = `
+                <iframe 
+                    src="https://silentspacemarine.com/" 
+                    style="width: 100%; height: 100%; border: none; background: #000;"
+                    allowfullscreen
+                    allow="autoplay; fullscreen; gamepad; clipboard-write">
+                </iframe>
+            `;
+        }
+    }, []);
 
     return (
         <Window
@@ -18,17 +31,21 @@ const DoomApp: React.FC<DoomAppProps> = (props) => {
             windowTitle="Doom"
             windowBarColor="#1C1C1C"
             windowBarIcon="windowGameIcon"
-            bottomLeftText={'Powered by JSDOS & DOSBox'}
+            bottomLeftText={'Powered by silentspacemarine.com'}
             closeWindow={props.onClose || (() => {})}
             onInteract={props.onInteract || (() => {})}
             minimizeWindow={props.onMinimize || (() => {})}
             onWidthChange={setWidth}
             onHeightChange={setHeight}
         >
-            <DosPlayerNew
-                width={width - 40} // Account for window padding
-                height={height - 80} // Account for window title bar and padding
-                bundleUrl="doom.jsdos"
+            <div 
+                ref={canvasRef}
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    background: '#000',
+                    border: 'none'
+                }}
             />
         </Window>
     );
