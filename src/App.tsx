06214957b4dbbbe08fd767ab68ea2ace6a207451
Jennifer Amaya <vane_amaya20@hotmail.es>
2025-07-14 12:48:35 -0400

    FINAL FIX: Deploy working files directly, no build process

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-// import Desktop from './components/os/Desktop';
+import Desktop from './components/os/Desktop';
 
 function App() {
     return (
         <div className="App">
-            <h1>Test - Portfolio V2</h1>
-            <p>If you see this, React is working!</p>
-            {/* <Desktop /> */}
+            <Desktop />
         </div>
     );
 }
